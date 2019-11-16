8f57565a2d2c0d0aabac87e1c6e923a727f63290
alanfl <alanfl81199@gmail.com>
2019-11-16 16:38:27 -0500

    Fix issue with logo rendering, fix issue with user table denying NULL values for nullable fields.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
+  `first_name` varchar(255),
   `middle_name` varchar(255),
-  `last_name` varchar(255) NOT NULL,
+  `last_name` varchar(255),
   PRIMARY KEY (username)
 );
 
