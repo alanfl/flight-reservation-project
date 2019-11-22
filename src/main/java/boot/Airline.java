a19796118b417a8c7e67a13f2aeca2e947e17cbe
Michael Flores <46034284+michaelflore@users.noreply.github.com>
2019-11-22 16:35:09 -0500

    Update Airline.java

--- src/main/java/boot/Airline.java
+++ src/main/java/boot/Airline.java
@@ -1,26 +1,26 @@
 package boot;
 
 public class Airline {
-    private String airline_Id;
-    private String airline_Name;
+    private String airline_id;
+    private String airline_name;
     
     public Airline() {
     }
     
-    public Airline(String airline_Id) {
-        this.airline_Id = airline_Id;
+    public Airline(String airline_id) {
+        this.airline_id = airline_id;
     }
     
-    public Airline(String airline_Id, String airline_Name) {
-        this.airline_Id = airline_Id;
-        this.airline_Name = airline_Name;
+    public Airline(String airline_id, String airline_name) {
+        this.airline_id = airline_id;
+        this.airline_name = airline_name;
     }
     
-    public String getAirline_Id() {
-        return airline_Id;
+    public String getAirline_id() {
+        return airline_id;
     }
     
-    public String getAirline_Name() {
-        return airline_Name;
+    public String getAirline_name() {
+        return airline_name;
     }
 }
