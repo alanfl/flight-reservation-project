07fc5ba20abc0cdb687870b625e17dbda3eb3832
schembora <schembora@gmail.com>
2019-11-30 12:41:53 -0500

    Added ticket and updated reservation files based on schema updates

---
+++ src/main/java/boot/TicketService.java
@@ -0,0 +1,93 @@
+package boot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    public Ticket get(String ticket_id){
+        return (Ticket) jdbc.queryForObject("SELECT * FROM ticket WHERE ticket_id = ?",
+                new Object[] { ticket_id },
+                (rs,rowNum) -> new Ticket(
+                        rs.getString("ticket_id"),
+                        rs.getString("reservation_id"),
+                        rs.getInt("leg_id"),
+                        rs.getString("airline_id"),
+                        rs.getInt("flight_id"),
+                        rs.getInt("departure_weekday"),
+                        rs.getDate("departure_date"),
+                        rs.getDouble("price"),
+                        rs.getString("waitlist_status")
+                )
+        );
+    }
+
+    public void delete(String ticket_id){
+        jdbc.update("DELETE FROM ticket WHERE ticket_id=?", ticket_id);
+    }
+
+    public Ticket save(Ticket ticket){
+        jdbc.update("INSERT INTO ticket VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                ticket.getTicketId(),
+                ticket.getReservationId(),
+                ticket.getLegId(),
+                ticket.getAirlineId(),
+                ticket.getFlightId(),
+                ticket.getDepartureWeekday(),
+                ticket.getDepartureDate(),
+                ticket.getPrice(),
+                ticket.getWaitlistStatus());
+        return ticket;
+    }
+
+    public Ticket update(Ticket ticket){
+        jdbc.update("UPDATE ticket SET reservation_id=?, leg_id=?, airline_id=?, flight_id=?, departure_weekday=?, departure_date=?, price=?, waitlist_status=? WHERE ticket_id=?",
+                ticket.getReservationId(),
+                ticket.getLegId(),
+                ticket.getAirlineId(),
+                ticket.getFlightId(),
+                ticket.getDepartureWeekday(),
+                ticket.getDepartureDate(),
+                ticket.getPrice(),
+                ticket.getWaitlistStatus(),
+                ticket.getTicketId());
+        return ticket;
+    }
+
+    public Iterable<Ticket> searchTickets(String[] ticket_ids){
+        if (ticket_ids != null){
+            return jdbc.query("SELECT * FROM ticket WHERE ticket_id IN ?",
+                    new Object[] { ticket_ids },
+                    (rs,rowNum) -> new Ticket(
+                            rs.getString("ticket_id"),
+                            rs.getString("reservation_id"),
+                            rs.getInt("leg_id"),
+                            rs.getString("airline_id"),
+                            rs.getInt("flight_id"),
+                            rs.getInt("departure_weekday"),
+                            rs.getDate("departure_date"),
+                            rs.getDouble("price"),
+                            rs.getString("waitlist_status")
+                    ));
+        }
+        else{
+            return jdbc.query("SELECT * FROM ticket",
+                    new Object[]{},
+                    (rs,rowNum) -> new Ticket(
+                        rs.getString("ticket_id"),
+                        rs.getString("reservation_id"),
+                        rs.getInt("leg_id"),
+                        rs.getString("airline_id"),
+                        rs.getInt("flight_id"),
+                        rs.getInt("departure_weekday"),
+                        rs.getDate("departure_date"),
+                        rs.getDouble("price"),
+                        rs.getString("waitlist_status")
+                    ));
+        }
+    }
+
+}
