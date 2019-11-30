11105d42ff28dc9b5433607824a2a844e8bcaa2f
alanfl <alanfl81199@gmail.com>
2019-11-30 04:40:34 -0500

    Remove unnecessary table, redefine attribute types for UUID libraries on front-end.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,19 +1,16 @@
-DROP TABLE IF EXISTS 'user';
-DROP TABLE IF EXISTS 'role';
-DROP TABLE IF EXISTS 'airline';
-DROP TABLE IF EXISTS 'aircraft';
-DROP TABLE IF EXISTS 'airport';
-DROP TABLE IF EXISTS 'airline_airport';
-DROP TABLE IF EXISTS 'flight';
-DROP TABLE IF EXISTS 'reservation';
-DROP TABLE IF EXISTS 'ticket';
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `role`;
+DROP TABLE IF EXISTS `airline`;
+DROP TABLE IF EXISTS `aircraft`;
+DROP TABLE IF EXISTS `airport`;
+DROP TABLE IF EXISTS `aircraft_capacity`;
+DROP TABLE IF EXISTS `flight`;
+DROP TABLE IF EXISTS `reservation`;
+DROP TABLE IF EXISTS `ticket`;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `first_name` varchar(255),
-  `middle_name` varchar(255),
-  `last_name` varchar(255),
   PRIMARY KEY (username)
 );
 
@@ -42,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `aircraft_capacity` (
   `aircraft_id` varchar(32) NOT NULL,
   `seat_class` varchar(32) NOT NULL,
   `capacity` int NOT NULL,
-  PRIMARY KEY (aircraft_id, seat_class, capacity),
+  PRIMARY KEY (aircraft_id, seat_class),
   FOREIGN KEY (aircraft_id) REFERENCES aircraft(aircraft_id)
 )
 
@@ -52,60 +49,52 @@ CREATE TABLE IF NOT EXISTS `airport` (
   PRIMARY KEY (airport_id)
 );
 
-CREATE TABLE IF NOT EXISTS `airline_airport` (
-  `airline_id` char(2) NOT NULL,
-  `airport_id` char(3) NOT NULL,
-  PRIMARY KEY (airline_id, airport_id),
-  FOREIGN KEY (airline_id) REFERENCES airline(airline_id),
-  FOREIGN KEY (airport_id) REFERENCES airport(airport_id)
-);
-
 CREATE TABLE IF NOT EXISTS `flight` (
   `flight_id` int NOT NULL,
   `airline_id` char(2) NOT NULL,
   `departure_airport_id` char(3) NOT NULL,
   `arrival_airport_id` char(3) NOT NULL,
-  `departure_weekday` varchar(16) NOT NULL,
-  `arrival_weekday` varchar(16) NOT NULL,
+  `departure_weekday` int NOT NULL,
+  `arrival_weekday` int NOT NULL,
   `aircraft_id` varchar(32) NOT NULL,
   `departure_time` time,
   `arrival_time` time,
   `price` DECIMAL(9, 2) NOT NULL,
-  `seats_remaining` int NOT NULL,
   PRIMARY KEY (airline_id, flight_id, departure_weekday),
   FOREIGN KEY (departure_airport_id) REFERENCES airport(airport_id),
   FOREIGN KEY (arrival_airport_id) REFERENCES airport(airport_id),
+  FOREIGN KEY (airline_id) REFERENCES airline(airline_id),
   FOREIGN KEY (aircraft_id) REFERENCES aircraft(aircraft_id)
 );
 
 CREATE TABLE IF NOT EXISTS `reservation` (
-  `reservation_id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int NOT NULL,
+  `reservation_id` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `origin_airport_id` char(3) NOT NULL,
   `purchase_date` date NOT NULL,
   `purchase_time` time NOT NULL,
   `departure_date` date NOT NULL,
   `departure_time` time NOT NULL,
   `total_fare` DECIMAL(11, 2) NOT NULL,
-  `fee` DECIMAL(11, 2) NOT NULL,
-  `special_meal` varchar(255),
-  `class` varchar(255) NOT NULL,
-  `reservation_status` varchar(255) NOT NULL,
+  `fee` DECIMAL(9, 2) NOT NULL,
+  `special_meal` varchar(32),
+  `seat_class` varchar(32) NOT NULL,
+  `booking_status` varchar(32) NOT NULL,
   PRIMARY KEY (reservation_id),
   FOREIGN KEY (origin_airport_id) REFERENCES airport(airport_id)
 );
 
 CREATE TABLE IF NOT EXISTS `ticket` (
-  `ticket_id` int NOT NULL,
-  `reservation_id` int NOT NULL,
+  `ticket_id` varchar(255) NOT NULL,
+  `reservation_id` varchar(255) NOT NULL,
   `leg_id` int NOT NULL,
   `airline_id` char(3) NOT NULL,
   `flight_id` int NOT NULL,
-  `departure_weekday` varchar(16) NOT NULL,
+  `departure_weekday` int NOT NULL,
   `departure_date` date NOT NULL,
   `price` DECIMAL(11, 2) NOT NULL,
-  `waitlist_status` varchar(16) NOT NULL,
-  PRIMARY KEY (ticket_id, leg_id),
+  `waitlist_status` varchar(32) NOT NULL,
+  PRIMARY KEY (reservation_id, ticket_id, leg_id),
   FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id),
   FOREIGN KEY (airline_id, flight_id, departure_weekday) REFERENCES flight(airline_id, flight_id, departure_weekday)
 );
