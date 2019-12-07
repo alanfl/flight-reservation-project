0c5a7f94f0b2ad855a3a851e81f137ea50b44de5
alanfl <alanfl81199@gmail.com>
2019-12-07 00:43:28 -0500

    Fix incorrect column name in ReservationService.java

--- src/main/java/boot/ReservationService.java
+++ src/main/java/boot/ReservationService.java
@@ -29,12 +29,12 @@ public class ReservationService {
     }
 
     public Reservation getById(String id) {
-        return (Reservation)jdbc.queryForObject("SELECT reservation_id, username, departure_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE reservation_id=? ORDER BY purchase_date DESC, purchase_time DESC", 
+        return (Reservation)jdbc.queryForObject("SELECT reservation_id, username, origin_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE reservation_id=? ORDER BY purchase_date DESC, purchase_time DESC", 
             new Object[] { id }, // arguments as array
             (rs, rowNum) -> new Reservation(
                 rs.getString("reservation_id"), 
                 rs.getString("username"), 
-                rs.getString("departure_airport_id"), 
+                rs.getString("origin_airport_id"), 
                 rs.getString("purchase_date"), 
                 rs.getString("purchase_time"), 
                 rs.getString("departure_date"), 
@@ -49,12 +49,12 @@ public class ReservationService {
     }
     
     public Iterable<Reservation> getByUsername(String username) {
-        return jdbc.query("SELECT reservation_id, username, departure_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE username=? ORDER BY purchase_date DESC, purchase_time DESC", 
+        return jdbc.query("SELECT reservation_id, username, origin_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE username=? ORDER BY purchase_date DESC, purchase_time DESC", 
             new Object[] { username }, // arguments as array
             (rs, rowNum) -> new Reservation(
                 rs.getString("reservation_id"), 
                 rs.getString("username"), 
-                rs.getString("departure_airport_id"), 
+                rs.getString("origin_airport_id"), 
                 rs.getString("purchase_date"), 
                 rs.getString("purchase_time"), 
                 rs.getString("departure_date"), 
@@ -102,12 +102,12 @@ public class ReservationService {
     }
 
     public Iterable<Reservation> searchByUsername(String username) {
-        return jdbc.query("SELECT reservation_id, username, departure_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE username=? ORDER BY purchase_date DESC, purchase_time DESC", 
+        return jdbc.query("SELECT reservation_id, username, origin_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE username=? ORDER BY purchase_date DESC, purchase_time DESC", 
             new Object[] { username }, // arguments as array
             (rs, rowNum) -> new Reservation(
                 rs.getString("reservation_id"), 
                 rs.getString("username"), 
-                rs.getString("departure_airport_id"), 
+                rs.getString("origin_airport_id"), 
                 rs.getString("purchase_date"), 
                 rs.getString("purchase_time"), 
                 rs.getString("departure_date"), 
@@ -122,12 +122,12 @@ public class ReservationService {
     }
 
     public Iterable<Reservation> searchById(String id) {
-        return jdbc.query("SELECT reservation_id, username, departure_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE reservation_id=? ORDER BY purchase_date DESC, purchase_time DESC", 
+        return jdbc.query("SELECT reservation_id, username, origin_airport_id, purchase_date, purchase_time, departure_date, departure_time, total_fare, fee, special_meal, seat_class, booking_status FROM reservation WHERE reservation_id=? ORDER BY purchase_date DESC, purchase_time DESC", 
             new Object[] { id }, // arguments as array
             (rs, rowNum) -> new Reservation(
                 rs.getString("reservation_id"), 
                 rs.getString("username"), 
-                rs.getString("departure_airport_id"), 
+                rs.getString("origin_airport_id"), 
                 rs.getString("purchase_date"), 
                 rs.getString("purchase_time"), 
                 rs.getString("departure_date"), 
@@ -143,12 +143,12 @@ public class ReservationService {
     
     public Iterable<Reservation> searchByFlight(
         String airline_id, String flight_id, String departure_date) {
-        return jdbc.query("SELECT DISTINCT r.reservation_id, r.username, r.departure_airport_id, r.purchase_date, r.purchase_time, r.departure_date, r.departure_time, r.total_fare, r.fee, r.special_meal, r.seat_class, r.booking_status FROM reservation r, ticket t WHERE t.airline_id=? AND t.flight_id=? AND t.departure_date=? AND r.reservation_id=t.reservation_id ORDER BY r.purchase_date DESC, r.purchase_time DESC", 
+        return jdbc.query("SELECT DISTINCT r.reservation_id, r.username, r.origin_airport_id, r.purchase_date, r.purchase_time, r.departure_date, r.departure_time, r.total_fare, r.fee, r.special_meal, r.seat_class, r.booking_status FROM reservation r, ticket t WHERE t.airline_id=? AND t.flight_id=? AND t.departure_date=? AND r.reservation_id=t.reservation_id ORDER BY r.purchase_date DESC, r.purchase_time DESC", 
             new Object[] { airline_id, flight_id, departure_date }, // arguments as array
                 (rs, rowNum) -> new Reservation(
                     rs.getString("reservation_id"), 
                     rs.getString("username"), 
-                    rs.getString("departure_airport_id"), 
+                    rs.getString("origin_airport_id"), 
                     rs.getString("purchase_date"), 
                     rs.getString("purchase_time"), 
                     rs.getString("departure_date"), 
